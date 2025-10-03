4e09322b80b758e4bd8d69e0d3393c1ef1247631
makhil006 <makhil006@gmail.com>
2025-10-03 14:52:18 +0530

    removing unwanted files

--- src/secure_mcp_gateway/services/auth/__init__.py
+++
@@ -1,4 +0,0 @@
-# Legacy auth_service has been replaced by the plugin-based authentication system
-# Use: from secure_mcp_gateway.plugins.auth import get_auth_config_manager
-
-__all__ = []
--- src/secure_mcp_gateway/services/guardrails/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Enkrypt Secure MCP Gateway Guardrails Package
-
-This package hosts guardrail plugin providers and related helpers.
-Legacy `guardrail_service` is removed in favor of plugin system.
-"""
-
-__all__: list[str] = []
