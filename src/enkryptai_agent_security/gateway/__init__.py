76d23d7a1e738dbdabbb1fbcaff1fecd83931e09
Nitin A B <nitin.abirur@gmail.com>
2026-03-09 13:44:07 +0530

    Added lazy imports for modules

--- src/enkryptai_agent_security/gateway/__init__.py
+++ src/enkryptai_agent_security/gateway/__init__.py
@@ -2,6 +2,13 @@ import sys
 
 # TODO: Fix error and use stdout
 # print("Initializing Enkrypt Secure MCP Gateway", file=sys.stderr)
-from enkryptai_agent_security.gateway.client import *
-from enkryptai_agent_security.gateway.gateway import *
-from enkryptai_agent_security.gateway.utils import *
+
+# Gateway deps (redis, flask, etc.) are only available when installed with
+# the [gateway] extra. Guard these imports so that hooks-only installs can
+# still import this package and use the CLI without crashing.
+try:
+    from enkryptai_agent_security.gateway.client import *
+    from enkryptai_agent_security.gateway.gateway import *
+    from enkryptai_agent_security.gateway.utils import *
+except ImportError:
+    pass
